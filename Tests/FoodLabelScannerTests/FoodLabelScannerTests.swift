d12d04037ac691d37360685e4dcde83d234720c7
Ahmed Khalaf <pxlshpr@gmail.com>
2022-09-30 18:48:50 +0500

    bump

--- Tests/FoodLabelScannerTests/FoodLabelScannerTests.swift
+++ Tests/FoodLabelScannerTests/FoodLabelScannerTests.swift
@@ -3,7 +3,10 @@ import XCTest
 
 final class FoodLabelScannerTests: XCTestCase {
     func test() throws {
-        
+        guard let path = Bundle.module.path(forResource: "083C5BAA-2DDA-42E5-8A6C-DCD1A3E5B7E1", ofType: "jpg") else {
+//            XCTFail("Couldn't get path for \"\(testCaseFileType.fileName(for: testCase))\" for testCaseFileType: \(testCaseFileType.rawValue)")
+            return
+        }
         XCTAssertEqual("HI", "H")
     }
 }
