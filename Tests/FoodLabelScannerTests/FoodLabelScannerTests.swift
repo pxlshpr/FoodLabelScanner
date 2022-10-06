5b0b4e5e7e3166240ebf95cf5062bb6242418994
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-06 09:50:16 +0500

    bump

--- Tests/FoodLabelScannerTests/FoodLabelScannerTests.swift
+++ Tests/FoodLabelScannerTests/FoodLabelScannerTests.swift
@@ -5,7 +5,7 @@ import PrepUnits
 
 final class FoodLabelScannerTests: XCTestCase {
     func test() async throws {
-        guard let path = Bundle.module.path(forResource: "label10", ofType: "jpg"),
+        guard let path = Bundle.module.path(forResource: "toblerone", ofType: "jpg"),
               let image = UIImage(contentsOfFile: path)
         else {
             XCTFail("Couldn't get image")
@@ -16,5 +16,20 @@ final class FoodLabelScannerTests: XCTestCase {
         let values = FoodLabelValue.detect(in: string)
         print("Got: \(values)")
         let results = try await FoodLabelScanner(image: image).scan()
+        print("🧬 Protein was: \(results.protein)")
+    }
+}
+
+extension ScanResult {
+    var protein: FoodLabelValue? {
+        protein1
+    }
+    
+    var protein1: FoodLabelValue? {
+        nutrients.rows.first(where: { $0.attribute == .protein })?.value1
+    }
+
+    var protein2: FoodLabelValue? {
+        nutrients.rows.first(where: { $0.attribute == .protein })?.value2
     }
 }
