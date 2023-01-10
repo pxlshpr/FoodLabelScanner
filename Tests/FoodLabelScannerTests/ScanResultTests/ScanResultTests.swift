4e077544117159400fd2a3f748aa0a2990ed6808
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-10 19:07:43 +0500

    bump

--- Tests/FoodLabelScannerTests/ScanResultTests/ScanResultTests.swift
+++ Tests/FoodLabelScannerTests/ScanResultTests/ScanResultTests.swift
@@ -6,8 +6,7 @@ import PrepDataTypes
 final class ScanResultTests: XCTestCase {
     
     func testScanResult() async throws {
-        print("Let begin! 🏁")
-//        let path = Bundle.module.path(forResource: "peanut_butter_barcode", ofType: "jpg"),
+//        if let path = Bundle.module.path(forResource: "6676DCB9-1769-4070-B831-CC40B428AF72", ofType: "jpg")
 //        let image = UIImage(contentsOfFile: path)
         
         
@@ -15,4 +14,16 @@ final class ScanResultTests: XCTestCase {
         ///
                 
     }
+}
+
+func prepareTestCases() throws {
+    let filePath = Bundle.module.url(forResource: "NutritionClassifier-Test_Data", withExtension: "zip")!
+    let testDataUrl = URL.documents.appendingPathComponent("Test Data", isDirectory: true)
+    
+    /// Remove directory and create it again
+    try FileManager.default.removeItem(at: testDataUrl)
+    try FileManager.default.createDirectory(at: testDataUrl, withIntermediateDirectories: true)
+
+    /// Unzip Test Data contents
+    try Zip.unzipFile(filePath, destination: testDataUrl, overwrite: true, password: nil)
 }
