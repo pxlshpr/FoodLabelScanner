807b8d68fc303b621dbff8eef31621917b8e6c2e
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-10 19:57:37 +0500

    bump

--- Tests/FoodLabelScannerTests/ScanResultTests/ScanResultTests+Helpers.swift
+++ Tests/FoodLabelScannerTests/ScanResultTests/ScanResultTests+Helpers.swift
@@ -1,22 +1,84 @@
 import Foundation
+import FoodLabelScanner
+import VisionSugar
+import UIKit
 
-var testImagesDirectory: URL {
-    URL.documents.appendingPathComponent("Test Images", isDirectory: true)
-}
-
-var testCaseIds: [UUID] {
-    let files: [URL]
-    do {
-        files = try FileManager.default.contentsOfDirectory(
-            at: testImagesDirectory,
-            includingPropertiesForKeys: nil
-        )
-    } catch {
-        print("Error getting Test Case Files: \(error)")
-        files = []
+var testCases: [TestCase] {
+    guard let urls = Bundle.module.urls(forResourcesWithExtension: nil, subdirectory: "Test Cases") else {
+        fatalError("Couldn't get test case urls")
     }
-    let ids = files.compactMap { UUID(uuidString: $0.lastPathComponent.replacingOccurrences(of: ".jpg", with: "")) }
-    print("\(ids.count) test cases")
-    return ids
+    return urls.map { TestCase(url: $0) }
 }
 
+struct TestCase {
+    let url: URL
+    
+    var expectedScanResult: ScanResult {
+        get throws {
+            let data = try dataForFile(named: "scanResult", withExtension: "json")
+            return try JSONDecoder().decode(ScanResult.self, from: data)
+        }
+    }
+    
+    var expectedTextSet: RecognizedTextSet {
+        get throws {
+            let data = try dataForFile(named: "textSet", withExtension: "json")
+            return try JSONDecoder().decode(RecognizedTextSet.self, from: data)
+        }
+    }
+    
+    var actualScanResultFromActualTextSet: ScanResult {
+        get async throws {
+            let textSet = try await self.actualTextSet
+            return textSet.scanResult
+        }
+    }
+
+    var actualScanResultFromExpectedTextSet: ScanResult {
+        get async throws {
+            let textSet = try self.expectedTextSet
+            return textSet.scanResult
+        }
+    }
+
+    var actualTextSet: RecognizedTextSet {
+        get async throws {
+            let image = try self.image
+            return try await image.recognizedTextSet(for: .accurate, includeBarcodes: true)
+        }
+    }
+    
+    var image: UIImage {
+        get throws {
+            let data = try dataForFile(named: "image", withExtension: "png")
+            guard let image = UIImage(data: data) else {
+                throw ScanResultTestError.couldNotLoadImage
+            }
+            return image
+        }
+    }
+    
+    //MARK: Convenience
+    
+    func dataForFile(named name: String, withExtension ext: String) throws -> Data {
+        guard let scanResultFile = Bundle.module.url(
+            forResource: name,
+            withExtension: ext,
+            subdirectory: subdirectory
+        ) else {
+            fatalError("Couldn't get \(name).\(ext) for: \(id)")
+        }
+        return try Data(contentsOf: scanResultFile)
+    }
+
+    var id: String {
+        url.lastPathComponent
+    }
+    var subdirectory: String {
+        "Test Cases/\(id)"
+    }
+}
+
+enum ScanResultTestError: Error {
+    case couldNotLoadImage
+}
--- Tests/FoodLabelScannerTests/ScanResultTests/ScanResultTests.swift
+++ Tests/FoodLabelScannerTests/ScanResultTests/ScanResultTests.swift
@@ -5,31 +5,18 @@ import PrepDataTypes
 
 final class ScanResultTests: XCTestCase {
     
+    
     func testScanResult() async throws {
-//        if let path = Bundle.module.path(forResource: "6676DCB9-1769-4070-B831-CC40B428AF72", ofType: "jpg")
-//        let paths = Bundle.module.paths(forResourcesOfType: "jpg", inDirectory: nil)
-//        let urls = Bundle.module.urls(forResourcesWithExtension: nil, subdirectory: "TestCases.bundle")
-        let paths = Bundle.module.paths(forResourcesOfType: nil, inDirectory: "Test Cases")
-        print(paths)
+        for testCase in testCases {
+            let expectedScanResult = try testCase.expectedScanResult
+            print("We have \(expectedScanResult.nutrients.rows.count) nutrients to check")
+            
+            let expectedTextSet = try testCase.expectedTextSet
+            print("We have \(expectedTextSet.texts.count) texts")
+            
+            let image = try testCase.image
+            print("We have an image of dimensions: \(image.size)")
+        }
         
-        let urls = Bundle
-//        let image = UIImage(contentsOfFile: path)
-        
-        
-        /// Get all our test cases
-        ///
-                
     }
-}
-
-func prepareTestCases() throws {
-//    let filePath = Bundle.module.url(forResource: "NutritionClassifier-Test_Data", withExtension: "zip")!
-//    let testDataUrl = URL.documents.appendingPathComponent("Test Data", isDirectory: true)
-//    
-//    /// Remove directory and create it again
-//    try FileManager.default.removeItem(at: testDataUrl)
-//    try FileManager.default.createDirectory(at: testDataUrl, withIntermediateDirectories: true)
-//
-//    /// Unzip Test Data contents
-//    try Zip.unzipFile(filePath, destination: testDataUrl, overwrite: true, password: nil)
 }
