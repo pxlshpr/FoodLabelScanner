f69daae306eeb4037997507566921817d2671f9c
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-25 19:51:49 +0500

    tag increment

--- Sources/FoodLabelScanner/Structs/ScanResult/ScanResult+Helpers.swift
+++ Sources/FoodLabelScanner/Structs/ScanResult/ScanResult+Helpers.swift
@@ -301,3 +301,35 @@ public extension ScanResult {
             .boundingBox
     }
 }
+
+public extension ScanResult {
+    var textsWithFoodLabelValues: [RecognizedText] {
+        texts.filter { $0.hasFoodLabelValues }
+    }
+
+    var textsWithoutFoodLabelValues: [RecognizedText] {
+        texts.filter { !$0.hasFoodLabelValues }
+    }
+}
+
+import VisionSugar
+import PrepDataTypes
+
+public extension RecognizedText {
+    
+    /**
+     Returns the first detected `FoodLabelValue` in the string and all its candidates, if present.
+     */
+    var firstFoodLabelValue: FoodLabelValue? {
+        string.detectedValues.first ?? (candidates.first(where: { !$0.detectedValues.isEmpty }))?.detectedValues.first
+    }
+    
+    /**
+     Returns true if the string or any of the other candidates contains `FoodLabelValues` in them.
+     */
+    var hasFoodLabelValues: Bool {
+        !string.detectedValues.isEmpty
+        || candidates.contains(where: { !$0.detectedValues.isEmpty })
+    }
+}
+
