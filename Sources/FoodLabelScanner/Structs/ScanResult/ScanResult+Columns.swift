6bbf800e84f024b68c36e71e9e3b0634d3296cec
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-29 14:54:40 +0500

    tag increment

--- Sources/FoodLabelScanner/Structs/ScanResult/ScanResult+Columns.swift
+++ Sources/FoodLabelScanner/Structs/ScanResult/ScanResult+Columns.swift
@@ -11,12 +11,27 @@ public extension ScanResult {
         return headerType.description.replacingFirstOccurrence(of: "Per ", with: "")
     }
     
+    var headerType1: HeaderType? { headers?.header1Type }
+    var headerType2: HeaderType? { headers?.header2Type }
+
     var headerTitle1: String {
-        guard let headerType = headers?.header1Type else { return "Column 1" }
-        return headerType.description
+        guard let headerType1 else {
+            if let headerType2, headerType2 != .perServing {
+                return "Per Serving"
+            } else {
+                return "Column 1"
+            }
+        }
+        return headerType1.description
     }
     var headerTitle2: String {
-        guard let headerType = headers?.header2Type else { return "Column 2" }
-        return headerType.description
+        guard let headerType2 else {
+            if let headerType1, headerType1 != .perServing {
+                return "Per Serving"
+            } else {
+                return "Column 2"
+            }
+        }
+        return headerType2.description
     }
 }
